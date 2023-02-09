a0f3af365bb46c65a7b7eb63b179fa1f2d035573
waeldalaous <wael.dalaous@supcom.tn>
2023-02-09 15:18:11 +0100

    fixed file

--- src/main/java/com/visualpathit/account/utils/ElasticsearchUtil.java
+++ src/main/java/com/visualpathit/account/utils/ElasticsearchUtil.java
@@ -1,7 +1,7 @@
 package com.visualpathit.account.utils;
 
 import java.net.InetSocketAddress;
-
+// comment added here !
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
