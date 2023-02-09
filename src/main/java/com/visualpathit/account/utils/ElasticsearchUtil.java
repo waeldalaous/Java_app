5108dff29b063584ecae41abd0f92fa7d2ccbe52
waeldalaous <wael.dalaous@supcom.tn>
2023-02-09 15:23:29 +0100

    add comment here

--- src/main/java/com/visualpathit/account/utils/ElasticsearchUtil.java
+++ src/main/java/com/visualpathit/account/utils/ElasticsearchUtil.java
@@ -1,7 +1,7 @@
 package com.visualpathit.account.utils;
 
 import java.net.InetSocketAddress;
-
+// we add a new comment here 
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
